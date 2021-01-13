863e05117ee03a4759c5532e3967e3299eff301a
Aayush Gupta <aayush.gupta@bison.howard.edu>
2021-01-13 15:34:25 -0500

    Added basic impletmenation of dft

--- sketch.js
+++ sketch.js
@@ -1,9 +1,19 @@
 let time = 0;
 let wave = [];
-let NUM_MARKERS = 20;
+let y = [];
+let fourierY;
+
+let NUM_MARKERS = 10;
 
 function setup() {
   createCanvas(600, 400);
+
+  let angle = 0;
+  for (let i = 0; i < 100; i++){
+    y[i] = 100*noise(angle);
+    angle += TWO_PI/100;
+  }
+  fourierY = dft(y);
 }
 
 function draw() {
@@ -13,18 +23,18 @@ function draw() {
   let x = 0;
   let y = 0; 
 
-  for (let i = 0; i < NUM_MARKERS; i++) {
+  for (let i = 0; i < fourierY.length; i++) {
     let prev_x = x; 
     let prev_y = y; 
-    
-    // Calculate the next number in the series. 
-    // 1, 3, 5, 7, 9 .. 
-    let n = i*2 + 1;
+
+    // Calculate the params. 
+    let freq = fourierY[i].freq;
+    let radius = fourierY[i].amp;
+    let phase = fourierY[i].phase;
 
     // Using fourier series formula. 
-    let radius = 50 * (4 / (n * PI))
-    x += radius * cos(n * time);
-    y += radius * sin(n * time);
+    x += radius * cos(freq * time + phase + HALF_PI);
+    y += radius * sin(freq * time + phase + HALF_PI);
 
     // Draw the circle centered at prev_x and prev_y. 
     stroke(255, 100);
@@ -36,10 +46,12 @@ function draw() {
     line(prev_x, prev_y, x, y);
   }
 
+  // Prepend the new y value to the wave array. 
+  wave.unshift(y);
+
   // Move the graph to the right. 
   translate(200, 0);
   line(x-200, y, 0, wave[0]);
-  wave.unshift(y);
 
   // Draw the graph using the final values of y. 
   beginShape();
@@ -50,7 +62,8 @@ function draw() {
   endShape();
 
   // Increment time. 
-  time += 0.05; 
+  const dt = TWO_PI/fourierY.length;
+  time += dt; 
 
   // Ensure the array does not go out of bounds. 
   if (wave.length > 250){

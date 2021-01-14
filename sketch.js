9396df93c6e4efdf36d541dac43102c39b344a46
Aayush Gupta <aayush.gupta@bison.howard.edu>
2021-01-13 21:37:32 -0500

    Completed implmentation of drawing machine.

--- sketch.js
+++ sketch.js
@@ -6,11 +6,26 @@ let y = [];
 let fourierY;
 let fourierX;
 
-function setup() {
-  createCanvas(800, 600);
-  
+const USER = 0;
+const FOURIER = 1;
+
+let drawing = [];
+let state = -1; 
+
+function mousePressed() {
+  state = USER; 
+  drawing = [];
+  x = [];
+  y = [];
+  time = 0;
+  path = [];
+}
+
+function mouseReleased() {
+  state = FOURIER;
+
   // Scale down the data set by factor of skip. 
-  const skip = 10; 
+  const skip = 1; 
   for (let i = 0; i < drawing.length; i += skip){
     x.push(drawing[i].x);
     y.push(drawing[i].y);
@@ -21,6 +36,10 @@ function setup() {
   // Sort by amplitude for better rendering using epicycles. 
   fourierX.sort((a, b) => b.amp - a.amp);
   fourierY.sort((a, b) => b.amp - a.amp);
+}
+
+function setup() {
+  createCanvas(800, 600);
 }
 
 function epiCycles(x, y, rotation, fourier){
@@ -52,34 +71,49 @@ function epiCycles(x, y, rotation, fourier){
 
 function draw() {
   background(0);
-  
-  // Calculate the next set of points in the path. 
-  let vx = epiCycles(width/2, 100, 0, fourierX);
-  let vy = epiCycles(100, height/2, HALF_PI, fourierY)
-  let v = createVector(vx.x, vy.y) 
 
-  // Prepend the new y value to the wave array. 
-  path.unshift(v);
+  if (state == USER) {
+    let point = createVector(mouseX - width/2, mouseY-height/2)
+    drawing.push(point);
 
-  // Draw line
-  line(vx.x, vx.y, v.x, v.y)
-  line(vy.x, vy.y, v.x, v.y)
+    stroke(255);
+    noFill();
+    beginShape();
+    for (let v of drawing) {
+      vertex(v.x + width/2, v.y+height/2);
+    }
+    endShape()
 
-  // Draw the graph using the final values of y. 
-  beginShape();
-  noFill();
-  for (let i = 0; i < path.length; i++){
-    vertex(path[i].x, path[i].y);
-  }
-  endShape();
+  } else if (state == FOURIER) {
 
-  // Increment time. 
-  const dt = TWO_PI/fourierY.length;
-  time += dt; 
+    // Calculate the next set of points in the path. 
+    let vx = epiCycles(width/2, 100, 0, fourierX);
+    let vy = epiCycles(100, height/2, HALF_PI, fourierY)
+    let v = createVector(vx.x, vy.y) 
 
-  // Reset when a full cycle is complete. 
-  if (time > TWO_PI) {
-    time = 0;
-    path = [];
+    // Prepend the new y value to the wave array. 
+    path.unshift(v);
+
+    // Draw line
+    line(vx.x, vx.y, v.x, v.y)
+    line(vy.x, vy.y, v.x, v.y)
+
+    // Draw the graph using the final values of y. 
+    beginShape();
+    noFill();
+    for (let i = 0; i < path.length; i++){
+      vertex(path[i].x, path[i].y);
+    }
+    endShape();
+
+    // Increment time. 
+    const dt = TWO_PI/fourierY.length;
+    time += dt; 
+
+    // Reset when a full cycle is complete. 
+    if (time > TWO_PI) {
+      time = 0;
+      path = [];
+    }
   }
 }
